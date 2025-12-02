95a097065f50c3c42df5ba85710cc921d4c92cb4
webkype <webkypecrm@gmail.com>
2025-12-02 22:19:34 +0530

    one-rupeese

--- src/app/components/PayWithRozorpay.tsx
+++ src/app/components/PayWithRozorpay.tsx
@@ -243,7 +243,7 @@ export const handleRazorpayPayment = async (
     }
     const options = {
       key: process.env.NEXT_PUBLIC_RAZORPAY_KEY!,
-      amount: grandTotal * 100,
+      amount: 1 * 100,
       currency: "INR",
       name: "ChallanPay",
       description: "Challan Payment",
