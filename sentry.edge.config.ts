c4f57846a595c5df4d08daf5f543518bdb86d2d4
webkype <webkypecrm@gmail.com>
2025-12-03 16:22:04 +0530

    with-senry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,22 +3,22 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs";
 
-// Sentry.init({
-//   dsn: "https://b01be706c1328fac51532ea1c25a70cb@o4510169699713024.ingest.us.sentry.io/4510170226229248",
+Sentry.init({
+  dsn: "https://b01be706c1328fac51532ea1c25a70cb@o4510169699713024.ingest.us.sentry.io/4510170226229248",
 
-//   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-//   // tracesSampleRate: 1,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  // tracesSampleRate: 1,
 
-//   tracesSampleRate: 0.1,
+  tracesSampleRate: 0.1,
 
-//   // Enable logs to be sent to Sentry
-//   enableLogs: true,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
-//   ignoreErrors: ["ResizeObserver loop limit exceeded", "SomeOtherError"],
+  ignoreErrors: ["ResizeObserver loop limit exceeded", "SomeOtherError"],
 
-//   // Enable sending user PII (Personally Identifiable Information)
-//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-//   sendDefaultPii: true,
-// });
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
