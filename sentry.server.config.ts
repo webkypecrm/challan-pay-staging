05e72d9dd8ffbd80d8219e93800070f08e611bca
webkype <webkypecrm@gmail.com>
2025-12-03 16:10:48 +0530

    sentry-remove

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,18 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://b01be706c1328fac51532ea1c25a70cb@o4510169699713024.ingest.us.sentry.io/4510170226229248",
+// Sentry.init({
+//   dsn: "https://b01be706c1328fac51532ea1c25a70cb@o4510169699713024.ingest.us.sentry.io/4510170226229248",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+//   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+//   tracesSampleRate: 1,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+//   // Enable logs to be sent to Sentry
+//   enableLogs: true,
 
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-});
+//   // Enable sending user PII (Personally Identifiable Information)
+//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+//   sendDefaultPii: true,
+// });
